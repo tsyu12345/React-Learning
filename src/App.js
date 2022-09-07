c629909d20991244e21e427757ac7412004ebe54
syu_asus_Note <syukunt@icloud.com>
2022-09-08 00:16:18 +0900

    第4回終了。propsについて。

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					HELLO FUCK REACT
-				</a>
-			</header>
+		<div id="root">
+			<h1>React App</h1>
+			<Button 
+				text="FUCK ME"
+			/>
 		</div>
 	);
 }
---
+++ src/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = (props) => {
+    return (
+        <button className="btn btn-primary">
+            {props.text}
+        </button>
+    );
+}
+
+export default Button;
